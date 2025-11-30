54f45c25f594c33f6d9c3c28678809ba12fb70ea
Ali-Abdullah-103 <aliabdullah.10306@gmail.com>
2025-11-30 16:57:49 +0500

    some changes

--- include/MetaDataParser.hpp
+++ include/MetaDataParser.hpp
@@ -10,9 +10,8 @@ class MetadataParser
 private:
     //There can be multiple sha for one document, we use only 1 for identification
     std::string extract_first_sha(const std::string& sha) const;
-public:
 
-    //parse a CSV line into fields (handles quoted commas)
+        //parse a CSV line into fields (handles quoted commas)
     void parse_line(const std::string&, std::vector<std::string>&);
 
     //finds fulltext JSON file path (checks pmcid first, then sha)
@@ -22,6 +21,9 @@ public:
     // Extract raw body text from JSON file
     std::string extract_text_from_json(const std::string& file_path) const;
 
+    //For tokenizing the text of the document to put in lexicon
+    std::vector<std::string> tokenize(const std::string& text) const;
+public:
     // Main parsing function: populates Lexicon, ForwardIndex, and InvertedIndex
     int metadata_parse(Lexicon& lex, ForwardIndex& fwd, InvertedIndex& inv, size_t max_docs);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,16 @@ int main() {
 
     MetadataParser parser;
 
-    size_t max_docs = 100; // Try with smaller batch first
+    size_t max_docs = 1000; //let's say process first 1000 docs
     int processed = parser.metadata_parse(lex, fwd, inv, max_docs);
 
-    std::cout << "\n=== Indexing Complete ===\n";
-    std::cout << "Documents processed: " << processed << "\n";
+    std::cout << "\n----- Indexing Complete -----\n";
+    std::cout << "Documents processed: " << processed << "\n\n";
     lex.show_statistics();
-    fwd.show_statistics();
+    std::cout << std::endl;
+    std::cout << "Total documents in Forward Index: " << fwd.total_documents() << "\n";
 
-    // ----- Save to files -----
+    //Save to files
     lex.save("D:/searchEngine/indices/lexicon.csv");
     fwd.save_to_file("D:/searchEngine/indices/forward_index.txt");
     inv.save_to_file("D:/searchEngine/indices/inverted_index.csv");
